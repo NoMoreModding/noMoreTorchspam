7ab814ef0a5c4fed2e4a3aebb20bb8738e457e4e
Tobias Filler <64466469+TobiasFiller@users.noreply.github.com>
2021-04-26 18:18:26 +0200

    new Glow Crystal texture and models - added glow_crystal_1.json glow_crystal_2.json glow_crystal_3.json glow_crystal_4.json - added Blockstate crystals - changed glow_crystal.png

--- src/main/java/com/hagenberg/fh/nomoretorchspam/common/block/GlowCrystal.java
+++ src/main/java/com/hagenberg/fh/nomoretorchspam/common/block/GlowCrystal.java
@@ -2,15 +2,28 @@ package com.hagenberg.fh.nomoretorchspam.common.block;
 
 import com.hagenberg.fh.nomoretorchspam.NoMoreTorchSpam;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.state.Property;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraftforge.common.extensions.IForgeBlockState;
+
+import javax.annotation.Nullable;
 
 // This is the block class of GlowCrystal
 
 public class GlowCrystal extends Block {
 
+    //Blockstate that provides the number of crystals in the block
+    public static final IntegerProperty CRYSTALS = IntegerProperty.create("crystals",1,4);
+
     public GlowCrystal(Properties properties) {
         super(properties);
-
     }
+
     //is called by EvListener when this has been placed
     public static void placed() {
         //TODO add placement of other blocks
@@ -26,5 +39,9 @@ public class GlowCrystal extends Block {
         }
     }
 
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(CRYSTALS);
+    }
 
 }
